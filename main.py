4d602462d4473c5361482abcf4861554471b0ff2
ycleo <leoyclee@gmail.com>
2021-11-16 00:57:00 +0800

    practice PIL, os, and sys modules

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import requests
+import sys
+import os
+from PIL import Image
 
-response = requests.get('https://httpbin.org/ip')
+# grab first and second arguments
+origin_folder = sys.argv[1]
+target_folder = sys.argv[2]
 
-print('Your IP is {0}'.format(response.json()['origin']))
+# check wether new/ exists, if not, create it
+if not os.path.isdir(target_folder):
+    os.makedirs(target_folder)
+
+# loop through pokemon folder and save to new/ folder
+for filename in os.listdir(origin_folder):
+    im = Image.open(f"{origin_folder}/{filename}")
+    clean_name = os.path.splitext(filename)[0]
+    im.save(f"{target_folder}/{clean_name}.png", "png")
+    print(f"{filename} has been converted to {clean_name}.png")
